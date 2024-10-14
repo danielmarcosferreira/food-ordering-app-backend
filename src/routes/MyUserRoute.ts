e794feedfbcec8d0541af5c0c2c5973cd4d938f3
danielmarcosferreira <daniel.marcos.1038@gmail.com>
2024-10-14 19:16:45 -0300

    feat:add auth and validation middlewares, update current user functionality and route

--- src/routes/MyUserRoute.ts
+++ src/routes/MyUserRoute.ts
@@ -1,10 +1,15 @@
 import express, { Request, Response } from "express"
 import MyUserController from "../controllers/MyUserController"
+import { jwtCheck, jwtParse } from "../middleware/auth";
+import { validateMyUserRequest } from "../middleware/validation";
 
 const router = express.Router()
 
-router.post("/", (req: Request, res: Response) => {
+router.post("/", jwtCheck, (req: Request, res: Response) => {
     MyUserController.createCurrentUser(req, res)
 })
+router.put("/", jwtCheck, jwtParse, validateMyUserRequest ,async (req: Request, res: Response): Promise<void> => {
+    await MyUserController.updateCurrentUser(req, res);
+});
 
 export default router;
