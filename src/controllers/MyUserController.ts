a03c228c005aee26a2077364ac4584d2cc897e2f
danielmarcosferreira <daniel.marcos.1038@gmail.com>
2024-10-16 05:38:08 -0300

    feat: fetch user data

--- src/controllers/MyUserController.ts
+++ src/controllers/MyUserController.ts
@@ -1,6 +1,23 @@
 import { Request, Response } from "express";
 import User from "../models/user";
 
+const getCurrentUser = async (req: Request, res: Response) => {
+    try {
+        const currentUser = await User.findOne({_id: req.userId})
+        if (!currentUser) {
+            res.status(404).json({message: "User not found"})
+            return
+        }
+
+        res.json(currentUser)
+        return
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({ message: "Something went wrong" })
+        return
+    }
+}
+
 const createCurrentUser = async (req: Request, res: Response) => {
     // 1. check if the user exists
     // 2. create the user if it doesn't exist
@@ -11,16 +28,19 @@ const createCurrentUser = async (req: Request, res: Response) => {
         const existingUser = await User.findOne({ auth0Id })
 
         if (existingUser) {
-            return res.status(200).send(existingUser.toObject());
+            res.status(200).send(existingUser.toObject());
+            return
         }
 
         const newUser = new User(req.body)
         await newUser.save();
 
         res.status(201).json(newUser.toObject())
+        return
     } catch (error) {
         console.log(error);
         res.status(500).json({ message: "Error creating user" })
+        return
     }
 }
 
@@ -30,7 +50,8 @@ const updateCurrentUser = async (req: Request, res: Response) => {
         const user = await User.findById(req.userId)
 
         if (!user) {
-            return res.status(404).json({ message: "User not found" })
+            res.status(404).json({ message: "User not found" })
+            return
         }
 
         user.name = name
@@ -41,13 +62,16 @@ const updateCurrentUser = async (req: Request, res: Response) => {
         await user.save()
 
         res.send(user)
+        return
     } catch (error) {
         console.log(error)
         res.status(500).json({ message: "Error updating user" })
+        return
     }
 }
 
 export default {
+    getCurrentUser,
     createCurrentUser,
     updateCurrentUser
 };
--- src/routes/MyUserRoute.ts
+++ src/routes/MyUserRoute.ts
@@ -5,11 +5,9 @@ import { validateMyUserRequest } from "../middleware/validation";
 
 const router = express.Router()
 
-router.post("/", jwtCheck, (req: Request, res: Response) => {
-    MyUserController.createCurrentUser(req, res)
-})
-router.put("/", jwtCheck, jwtParse, validateMyUserRequest ,async (req: Request, res: Response): Promise<void> => {
-    await MyUserController.updateCurrentUser(req, res);
-});
+router.get("/", jwtCheck, jwtParse, MyUserController.getCurrentUser)
+router.post("/", jwtCheck, MyUserController.createCurrentUser)
+router.put("/", jwtCheck, jwtParse, validateMyUserRequest, MyUserController.updateCurrentUser);
+
 
 export default router;
